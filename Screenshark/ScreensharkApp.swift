03f95ebb739e223deb3fb97c0db13608ce95beb4
devlim0210 <ftr.devlim@gmail.com>
2024-11-07 17:52:06 +0100

    Initial Commit

---
+++ Screenshark/ScreensharkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreensharkApp.swift
+//  Screenshark
+//
+//  Created by Ink.limited Ratsimaharison on 07/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScreensharkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScreensharkTests/ScreensharkTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScreensharkTests.swift
+//  ScreensharkTests
+//
+//  Created by Ink.limited Ratsimaharison on 07/11/2024.
+//
+
+import Testing
+@testable import Screenshark
+
+struct ScreensharkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
