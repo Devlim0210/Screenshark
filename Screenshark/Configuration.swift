8d5828f74b164f11efa18a82470e2072bda5d6a5
Devlim0210 <ftr.devlim@gmail.com>
2024-11-12 20:13:14 +0100

    Ajout du fichier Configuration.swift

---
+++ Screenshark/Configuration.swift
@@ -0,0 +1,11 @@
+//
+//  Configuration.swift
+//  Screenshark
+//
+//  Created by Ink.limited Ratsimaharison on 12/11/2024.
+//
+
+import Foundation
+struct Configuration {
+    static let apiKey = "8349cbe837def9ff97ed5e2c9234aada"
+}
---
+++ Screenshark/Movie.swift
@@ -0,0 +1,8 @@
+//
+//  Movie.swift
+//  Screenshark
+//
+//  Created by Ink.limited Ratsimaharison on 12/11/2024.
+//
+
+import Foundation
